b7052d7f2e4cd5208d80b4fc2cf140a2f39f5b02
Mirfayz077 <mirfayzxalloqov@gmail.com>
2026-04-16 15:36:27 +0500

    part end

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { Alpine, startLivewire } from './modules/livewire';
 import { registerThemePanel } from './modules/theme-panel';
 import { bootFlowbite } from './modules/flowbite';
 import { bootSwiper } from './modules/swiper';
+import './modules/stars'; // 👈 SHU QATOR MUHIM
 
 registerThemePanel(Alpine);
 bootFlowbite();
